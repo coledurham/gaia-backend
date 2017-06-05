0c5273964243313ab54757c1a26397962bca165e
Drew Durham <coledurham@gmail.com>
2017-06-05 09:26:09 -0600

    Added default route handler for invalid urls.

--- gbcc/routes/index.js
+++ gbcc/routes/index.js
@@ -8,4 +8,9 @@ router.get('/terms/:tid/longest-preview-media-url', function(req, res, next){
   api.processData(req, res);
 });
 
+router.get('*', function(req, res, next){
+  res.setHeader('Content-Type', 'text/html');
+  res.send('Unsupported URL');
+});
+
 module.exports = router;
